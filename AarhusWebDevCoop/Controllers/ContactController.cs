14187281e6d85ff3a168f54faeb53b888c73a345
soerenchristiansen <astonvilla83@hotmail.com>
2015-09-29 08:50:09 +0200

    first commit

---
+++ AarhusWebDevCoop/Controllers/ContactController.cs
@@ -0,0 +1,70 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult ContactForm()
+        {
+            var model = new ContactModel();
+            return PartialView("ContactForm", model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ContactForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Comment");
+
+                comment.SetValue("name", model.Name);
+                comment.SetValue("email", model.Email);
+                comment.SetValue("subject", model.Subject);
+                comment.SetValue("message", model.Message);
+
+                Services.ContentService.Save(comment);
+
+                var sb = new StringBuilder();
+                sb.AppendFormat("<p>Message: {0}</p>", model.Message);
+                sb.AppendFormat("<p>Subject: {0}</p>", model.Subject);
+                sb.AppendFormat("<p>Email: {0}</p>", model.Email);
+
+                var message = new MailMessage();
+
+                message.To.Add(new MailAddress(ConfigurationManager.AppSettings["ContactAddress"]));
+                message.Subject = model.Subject;
+                message.From = new MailAddress(model.Email, model.Name);
+                message.Body = sb.ToString();
+                message.IsBodyHtml = true;
+                TempData["success"] = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "astonvilla83@hotmail.com",  // replace with valid value
+                        Password = "carlos1"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                }
+
+                return RedirectToCurrentUmbracoPage();
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ AarhusWebDevCoop/ViewModels/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid email address")]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        
+    }
+}
